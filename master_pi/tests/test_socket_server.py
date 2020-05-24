62566e973dfe7b2bed3a4647037cd06f0dc92907
matt-samuel-s3717393 <s3717393@student.rmit.edu.au>
2020-05-24 19:04:52 +1000

    Added extra unit tests to socket server

--- master_pi/tests/test_socket_server.py
+++ master_pi/tests/test_socket_server.py
@@ -48,7 +48,8 @@ class TestSocketServer:
     @mock.patch('socket_server.server.socket.socket.accept')
     @mock.patch('socket_server.server.requests.post')
     def test_login(self, mock_post, mock_connection, socket_server):
-        """Tests that the instruction sent to the server is correct
+        """Tests that the socket calls the API endpoint for logging in and
+        returns it's response correctly
         """
         api_response = json.dumps({
             "user": "test",
@@ -71,7 +72,7 @@ class TestSocketServer:
         mock_connection.sendall.assert_called_with(api_response.encode())
 
     @mock.patch('socket_server.server.socket.socket.accept')
-    @mock.patch('socket_server.server.Server.recieve_image_from_client')
+    @mock.patch('socket_server.server.Server.receive_image_from_client')
     @mock.patch('socket_server.face_recognition_util.cv2.imwrite')
     def test_add_face(self, mock_cv2, mock_image, mock_connection, socket_server):
         """Tests to make sure that images are being saved
@@ -91,21 +92,78 @@ class TestSocketServer:
         mock_cv2.assert_called()
 
     @mock.patch('socket_server.server.socket.socket.accept')
-    @mock.patch('socket_server.server.Server.receive_image_from_client')
-    @mock.patch('socket_server.face_recognition_util.cv2.imwrite')
-    def test_add_face(self, mock_cv2, mock_image, mock_connection, socket_server):
-        """Tests to make sure that images are being saved
+    @mock.patch('socket_server.server.requests.post')
+    def test_return_car(self, mock_post, mock_connection, socket_server):
+        """Tests that the socket calls the API endpoint for returning a car and
+        returns it's response correctly
         """
+        api_response = json.dumps({
+            "message": "Car is returned"
+        })
+
         # Setting up mock objects
         mock_connection.return_value = (mock_connection, "127.0.0.1")
-        mock_image.return_value = {
+        mock_connection.recv.return_value = json.dumps({
             "username": "test",
-            "image": "image"
-        }
+            "car_id": "1",
+            "method": "return"
+        }).encode()
+        mock_post.status_code.return_value = 200
+        mock_post.return_value.text = api_response
 
         # Code to be tested
         socket_server.wait_for_connection()
-        socket_server.add_face()
+        response = socket_server.change_lock_status()
 
-        # Making sure that the function to save to the dataset folder is called
-        mock_cv2.assert_called()
+        mock_connection.sendall.assert_called_with(api_response.encode())
+
+    @mock.patch('socket_server.server.socket.socket.accept')
+    @mock.patch('socket_server.server.requests.post')
+    def test_unlock_car(self, mock_post, mock_connection, socket_server):
+        """Tests that the socket calls the API endpoint for unlocking a car and
+        returns it's response correctly
+        """
+        api_response = json.dumps({
+            "message": "Car is unlocked"
+        })
+
+        # Setting up mock objects
+        mock_connection.return_value = (mock_connection, "127.0.0.1")
+        mock_connection.recv.return_value = json.dumps({
+            "username": "test",
+            "car_id": "1",
+            "method": "unlock"
+        }).encode()
+        mock_post.status_code.return_value = 200
+        mock_post.return_value.text = api_response
+
+        # Code to be tested
+        socket_server.wait_for_connection()
+        response = socket_server.change_lock_status()
+
+        mock_connection.sendall.assert_called_with(api_response.encode())
+
+    @mock.patch('socket_server.server.socket.socket.accept')
+    @mock.patch('socket_server.server.requests.post')
+    def test_change_car_location(self, mock_post, mock_connection, socket_server):
+        """Tests that the socket calls the API endpoint for changing the location
+        of a car and returns it's response correctly
+        """
+        api_response = json.dumps({
+            "message": "Location has been changed"
+        })
+
+        # Setting up mock objects
+        mock_connection.return_value = (mock_connection, "127.0.0.1")
+        mock_connection.recv.return_value = json.dumps({
+            "car_id": "1",
+            "location": "return"
+        }).encode()
+        mock_post.status_code.return_value = 200
+        mock_post.return_value.text = api_response
+
+        # Code to be tested
+        socket_server.wait_for_connection()
+        response = socket_server.change_car_location()
+
+        mock_connection.sendall.assert_called_with(api_response.encode())
