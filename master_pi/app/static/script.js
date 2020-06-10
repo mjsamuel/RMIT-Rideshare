58cbedcaf09efeb69ac1dbc456686b159a24bc86
matt-samuel-s3717393 <s3717393@student.rmit.edu.au>
2020-06-10 23:40:40 +1000

    Refactored frontend components

--- master_pi/app/static/script.js
+++ master_pi/app/static/script.js
@@ -1,19 +1,40 @@
 function isLoggedIn() {
     if (sessionStorage.getItem('username') == null) {
-        window.location.href = "/login"
+        window.location.href = "/login";
     }
 }
 
 function logout() {
-  sessionStorage.removeItem('username')
-  window.location.href = "/login"
+  sessionStorage.removeItem('username');
+  sessionStorage.removeItem('role');
+  window.location.href = "/login";
+}
+
+function lockout(roles) {
+  // Locks out the page to only the roles specified
+  let userRole = sessionStorage.getItem('role')
+  var ableToAccess = false;
+  for (role in roles) {
+    if (userRole == roles[role]) ableToAccess = true;
+  }
+
+  if (ableToAccess == false) {
+    if (userRole == "default") redirect("");
+    else if (userRole == "admin") redirect("admin-console");
+    else if (userRole == "manager") redirect("manager-console");
+    else if (userRole == "engineer") redirect("engineer-console");
+  }
+}
+
+function redirect(url) {
+  window.location.href = "/" + url;
 }
 
 function outputErrors(errors) {
-  var output = "<strong>Error:</strong><ul>\n"
+  var output = "<strong>Error:</strong><ul>\n";
   for (e in errors) {
-    output += "<li>" + errors[e] + "</li>\n"
+    output += "<li>" + errors[e] + "</li>\n";
   }
-  output += "</ul>\n"
-  return output
+  output += "</ul>\n";
+  return output;
 }
