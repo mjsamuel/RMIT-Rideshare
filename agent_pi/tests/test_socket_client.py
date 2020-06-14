b606a620c59f017f57500838b2a716073948b97f
Oskar Floeck <oskar.floeck@gmail.com>
2020-06-14 18:44:16 +1000

    Adding test for qr code util and tidy up others.

--- agent_pi/tests/test_socket_client.py
+++ agent_pi/tests/test_socket_client.py
@@ -1,4 +1,4 @@
-import json, pytest, mock, pyqrcode, png, os
+import json, pytest, mock, pyqrcode, png, os, cv2
 from socket_client.client import Client
 from socket_client.qr_code_util import QrCodeUtil
 
@@ -46,51 +46,6 @@ class TestSocketClient:
 
     @mock.patch('socket_client.client.socket.socket')
     def test_login_bluetooth(self, mock_connection, socket_client):
-        server_response = {
-            'message': 'Logged in',
-            'user': 'test'
-        }
-        # Dummy mac address
-        mac_address = '18:F1:D8:E2:E9:6B'
-
-        # Setting up mock object
-        mock_connection.return_value = mock_connection
-        mock_connection.recv.return_value = json.dumps(server_response).encode()
-
-        # Code to be tested
-        socket_client.connect_to_server()
-        response = socket_client.login_via_bluetooth(mac_address)
-
-        # Checking values are as expected
-        assert (response == server_response)
-
-
-    @mock.patch('socket_client.client.socket.socket')
-    def test_login_qr_code(self, mock_connection, socket_client):
-        server_response = {
-            'message': 'Logged in',
-            'user': 'test'
-        }
-        # Dummy qr code creation
-        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-        image = open(os.path.join(__location__, 'test.png'), encoding="utf8", errors='ignore')
-        qr_util = QrCodeUtil()
-        data = qr_util.decode_qr_code(image)
-
-        # Setting up mock object
-        mock_connection.return_value = mock_connection
-        mock_connection.recv.return_value = json.dumps(server_response).encode()
-
-        # Code to be tested
-        socket_client.connect_to_server()
-        response = socket_client.login(data["username"], data["password"])
-
-        # Checking values are as expected
-        assert (response == server_response)
-
-
-    @mock.patch('socket_client.client.socket.socket')
-    def test_login_face(self, mock_connection, socket_client):
         server_response = {
             'message': 'Logged in',
             'user': 'test'
